38ebac804421daf5b105e954ff06624454177145
Steven M Noyes <stevenmnoyes.af@gmail.com>
2018-03-31 23:07:00 -0700

    Add files via upload

--- functions.js
+++ functions.js
@@ -5,3 +5,15 @@ function zipCode (z) {
     return "false";
   }
 }
+
+window.setTimeout(ShowBanners, 5000);
+
+var zipcoder = document.getElementById("zipcheck");
+
+zipcoder.addEventListener("click", function() {
+  if (zipcheck === 0){
+  alert("We've got your area covered.");
+  } else {
+    alert("Sorry, we haven't expanded to that area yet.");
+  }
+});
